ba1ec8dd5ad1917484c005436ba125650cd0751a
istoica <istoica@opentext.com>
2023-11-15 16:19:58 +0200

    rm wpf

--- WPF/FindFlight/Res4/Report/test_data_src.js
+++
@@ -1,1 +0,0 @@
-﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\farchi\\Downloads\\NEW-BPT-FOLDER\\BPTDOWNLOADER FILES\\DEFAULT_Demo\\Components\\WPF\\FindFlight\\Res4',namePairs:[]}]};
